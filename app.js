b3b91b0f5ac1cec5e0fa744fd3bb519968d50d97
Rentsen <rentsentsognemekh@Rentsens-MacBook-Pro.local>
2024-02-13 11:06:41 +0800

    shoo buudag bolgoson

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ var roundScore = 0;
 
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж өгнө. 
-var dice = Math.floor(Math.random() * 6) + 1;
-// window.document.querySelector('#score-0').textContent = dice;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
+
 
 
 // программ эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice")
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener('click', function () {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
+});
 
-document.querySelector(".dice").style.display = "none";
-
-console.log('шоо: ' + dice);
